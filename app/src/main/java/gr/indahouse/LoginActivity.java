356b0f7bf60500003f419b32baf58a57ef294df1
Lefkaditis Orestis <orestis_94@hotmail.com>
2022-06-06 15:50:24 +0300

    init comit in mac

--- app/src/main/java/gr/indahouse/LoginActivity.java
+++ app/src/main/java/gr/indahouse/LoginActivity.java
@@ -12,7 +12,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
